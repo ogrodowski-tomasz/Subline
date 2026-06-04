a14e46961dea5f0c1af56fd7daee8508f32b3ed9
Tomasz Ogrodowski <tom.ogrodowski@gmail.com>
2026-06-04 12:32:23 +0200

    Initial Commit

---
+++ Subline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Subline
+//
+//  Created by itcraft on 04/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Subline/SublineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SublineApp.swift
+//  Subline
+//
+//  Created by itcraft on 04/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SublineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
